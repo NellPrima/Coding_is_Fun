9ef0322dae0d53601277de60c691ddaeb6865af5
Chanell Cousin <nelltheowl@gmail.com>
2024-07-22 19:33:32 -0400

    fgdfgdg

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-//Import App
-
-import App from "./App";
-const root=ReactDOM.createRoot(document.getElementById ('root'));
-//ReactDOM.render(<App />, document.querySelector("#root"));
-
