a5e0b0a32711ace110e9a79014e64e3aac1a899c
Chanell Cousin <nelltheowl@gmail.com>
2024-07-21 22:20:13 -0400

    Lets figure this out tomorrow

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//Import App
+
+import App from "./App";
+const root=ReactDOM.createRoot(document.getElementById ('root'));
+//ReactDOM.render(<App />, document.querySelector("#root"));
+
