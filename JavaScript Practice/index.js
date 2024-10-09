b74cf0e5634e65526e2d8115f8fc0e86c46beb6d
Chanell Cousin <nelltheowl@gmail.com>
2024-10-09 00:47:21 -0400

    sync

--- JavaScript Practice/index.js
+++ JavaScript Practice/index.js
@@ -1,5 +1,15 @@
-const nameMe = document.querySelector(".name");
+const nothing = document.querySelector("#nothing");
+const something = document.querySelector("#something");
 
-function changeColor(color) {
-    nameMe.style.color = color;
+nothing.addEventListener("click", changeWords);
+
+function changeWords() {
+    //When I click on nothing I want the words to change to #something
+    document.getElementById("nothing") == ("something");
+    if (document.innerHTML === "Something")
+        document.getElementById("nothing").innerHTML = "Nothing";
+
 }
+
+
+ 
