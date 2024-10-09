64880fc82a14243e50c4cff1875a545209865216
Chanell Cousin <nelltheowl@gmail.com>
2024-10-09 19:33:44 -0400

    fksdjlksjf

--- JavaScript Practice/index.js
+++ JavaScript Practice/index.js
@@ -1,15 +1,24 @@
-const nothing = document.querySelector("#nothing");
+let nothing = document.querySelector("#nothing");
 const something = document.querySelector("#something");
 
 nothing.addEventListener("click", changeWords);
 
 function changeWords() {
-    //When I click on nothing I want the words to change to #something
-    document.getElementById("nothing") == ("something");
-    if (document.innerHTML === "Something")
-        document.getElementById("nothing").innerHTML = "Nothing";
+  nothing.textContent = something.textContent;
 
+  if (something.textContent === "Turns to Something") {
+    something.textContent = "Still results as nothing";
+  } else if (something.textContent === "Still results as nothing") {
+    something.textContent = "Will eventually turn to Something";
+  } else if (something.textContent === "Will eventually turn to Something") {
+    something.textContent = "Then turns back to";
+    //turn the text back to #nothing
+  } else if  (something.textContent === "Then turns back to") {
+    nothing.textContent = something.textContent;
+  }
+  
+
+  
+    
 }
 
-
- 
