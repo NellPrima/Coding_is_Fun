12a7761b4c4d88045eb6b7c7fa83cf86b193f137
Chanell Cousin <nelltheowl@gmail.com>
2024-10-11 18:08:58 -0400

    kldfjgldf

--- JavaScript Practice/index.js
+++ JavaScript Practice/index.js
@@ -1,4 +1,4 @@
-let nothing = document.querySelector("#nothing");
+const nothing = document.querySelector("#nothing");
 const something = document.querySelector("#something");
 
 nothing.addEventListener("click", changeWords);
@@ -6,19 +6,7 @@ nothing.addEventListener("click", changeWords);
 function changeWords() {
   nothing.textContent = something.textContent;
 
-  if (something.textContent === "Turns to Something") {
-    something.textContent = "Still results as nothing";
-  } else if (something.textContent === "Still results as nothing") {
-    something.textContent = "Will eventually turn to Something";
-  } else if (something.textContent === "Will eventually turn to Something") {
-    something.textContent = "Then turns back to";
-    //turn the text back to #nothing
-  } else if  (something.textContent === "Then turns back to") {
-    nothing.textContent = something.textContent;
-  }
-  
-
-  
-    
 }
+    
+
 
