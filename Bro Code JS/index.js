b7fc2ad44b8c846e5f7fc6138fe8331b6e09947f
Chanell Cousin <nelltheowl@gmail.com>
2024-10-20 21:14:03 -0400

    jslkdfs

---
+++ Bro Code JS/index.js
@@ -0,0 +1,8 @@
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+let gpaT = 4.5;
+
+console.log(`you are ${age} years old `);
+console.log(`The price is $${price} `)
+console.log(`my gpa is ${gpa} and yours is ${gpaT}`);
