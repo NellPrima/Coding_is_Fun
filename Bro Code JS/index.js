e8aa5e5dfb348f99390796775b7fdb2dc2cd25ef
Chanell Cousin <nelltheowl@gmail.com>
2024-10-22 14:42:41 -0400

    12-16 bro code videos are next

--- Bro Code JS/index.js
+++ Bro Code JS/index.js
@@ -1,10 +1,34 @@
-const PI = 3.14159;
-let radius;
-let circumference;
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit"); 
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult"); 
 
-radius = window.prompt('Enter the radius of a circle');
-radius = Number(radius);
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+        subResult.textContent = `You are subscribed!`;
 
-circumference = 2 * pi * radius;
+    }
+    else{
+         subResult.textContent = `You are NOT subscribed!`;
+    }
 
-console.log(circumference);
+    if(visaBtn.checked){
+        paymentResult.textContent = `You used a visa card to pay`;
+
+    }
+    else if(masterCardBtn.checked ){
+        paymentResult.textContent = `You used a master card to pay`;
+    }
+    else if(payPalBtn.checked ){
+        paymentResult.textContent = `You used PayPal to pay`;
+    }
+    else{
+        paymentResult.textContent = `You must select a payment type`;
+    }
+}
+
+
+
