1819f6c85aa66e2e1ce2f8a1490f0dacfa20542d
Chanell Cousin <nelltheowl@gmail.com>
2024-10-23 14:19:17 -0400

    brocode vids 17-21

--- Bro Code JS/index.js
+++ Bro Code JS/index.js
@@ -1,34 +1,21 @@
-const myCheckBox = document.getElementById("myCheckBox");
-const visaBtn = document.getElementById("visaBtn");
-const masterCardBtn = document.getElementById("masterCardBtn");
-const payPalBtn = document.getElementById("payPalBtn");
-const mySubmit = document.getElementById("mySubmit"); 
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult"); 
+function add(x, y){
+    result = x + y;
+    return result;
+}
 
-mySubmit.onclick = function(){
-    if(myCheckBox.checked){
-        subResult.textContent = `You are subscribed!`;
+let answer = add(2, 3);
+console.log(answer + 6);
 
+function isEven(number){
+    if(number % 2 === 0){
+        return true;
     }
-    else{
-         subResult.textContent = `You are NOT subscribed!`;
+    else {
+        return false;
     }
 
-    if(visaBtn.checked){
-        paymentResult.textContent = `You used a visa card to pay`;
-
-    }
-    else if(masterCardBtn.checked ){
-        paymentResult.textContent = `You used a master card to pay`;
-    }
-    else if(payPalBtn.checked ){
-        paymentResult.textContent = `You used PayPal to pay`;
-    }
-    else{
-        paymentResult.textContent = `You must select a payment type`;
-    }
 }
 
 
 
+console.log(isEven(10));
