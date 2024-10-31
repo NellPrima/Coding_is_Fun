99f10c8b95b99fa6e688822fd1437a3a6d74aa82
Chanell Cousin <nelltheowl@gmail.com>
2024-10-30 23:47:56 -0400

    almost done

--- Eloquent JS/practice.js
+++ Eloquent JS/practice.js
@@ -1,5 +1,43 @@
-let num = 0;
-while (num <= 100) {
-    console.log(num);
-    mum = num + 1;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 0;
+// while (num <= 99) {
+//     num = num +1;
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     console.log( "FizzBuzz");
+//   } else if (num % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (num % 3 === 0) {
+//     console.log( "Fizz");
+//   }
+//   else {
+//     console.log(num);
+//   }
+// }
+
+
+    
+ 
+
+// let x = 3;
+// let y = 3;
+
+// if (x && y === 3) {
+//     console.log("They are both equal to 3.");
+// }
+// else {
+//     console.log("butts");
+// };
