c28c8579b5546907aa98e4361ad0142a42260ac7
Chanell Cousin <nelltheowl@gmail.com>
2024-11-07 13:35:56 -0500

    sfsdf

--- Bro Code JS/index.js
+++ Bro Code JS/index.js
@@ -1,1 +1,9 @@
-console.log(true ? "butts" : "pants");
+function happyBirthday(username, username2, age) {
+    console.log(`Happy Birthday to you ${username} & ${username2}`);
+    console.log(`Happy Birthday to you`);
+    console.log(`Y'all smell like a POO!`);
+    console.log(`Happy Birthday to you`);
+    console.log(`You are both ${age} now!`);
+};
+
+happyBirthday("Matthew", "Nell", 32);
--- Eloquent JS/practice.js
+++ Eloquent JS/practice.js
@@ -60,21 +60,34 @@
 
 // console.log(addNumbers(5, 5));
 
-const hummus = function(factor) {
-    const ingredient = function(amount, unit, name) {
-        let ingredientAmount = amount * factor;
-        if (ingredientAmount > 1 ) {
-            unit += "s";
-        }
-        console.log(`${ingredientAmount} ${unit} ${name}`)
+// const hummus = function(factor = 1) {
+//     const ingredient = function(amount, unit, name) {
+//         let ingredientAmount = amount * factor;
+//         if (ingredientAmount > 1 ) {
+//             unit += "s";
+//         }
+//         console.log(`${ingredientAmount} ${unit} ${name}`)
 
-    };
-     ingredient(1, "can" , "chickpeas");
-     ingredient(0.25, "cup" , "tahini");
-     ingredient(0.25, "cup" , "lemon juice");
-     ingredient(1, "clove" , "garlic");
-     ingredient(2, "tablespoon" , "olive oil");
-     ingredient(0.5, "teaspoon" , "cumin");
-};
+//     };
+//      ingredient(1, "can" , "chickpeas");
+//      ingredient(0.25, "cup" , "tahini");
+//      ingredient(0.25, "cup" , "lemon juice");
+//      ingredient(1, "clove" , "garlic");
+//      ingredient(2, "tablespoon" , "olive oil");
+//      ingredient(0.5, "teaspoon" , "cumin");
+// };
 
-console.log(hummus(), ingredient());
+// console.log(hummus());
+
+// console.log("The future says:", future());
+
+// function future() {
+//     return "You'll never have flying cars.";
+// }
+
+function greet(who) {
+    console.log("Hello " + who);
+}
+
+greet("Harry");
+console.log("Bye");
